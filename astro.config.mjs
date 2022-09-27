370c564920e677a657042a619bfdbc4fce960507
Victor Ndaba <mwangindaba@gmail.com>
2022-09-27 23:38:45 +0300

    build: updated astro site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,32 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import mdx from "@astrojs/mdx";
-
 import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx(), image()]
+  site: "https://vndaba.rocks",
+  base: "/",
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+    mdx(),
+    image(),
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "./src"),
+      },
+    },
+  },
 });
