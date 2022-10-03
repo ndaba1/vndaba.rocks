14d406a7d772d39211f5af8e5cf2c744feb09cab
Victor Ndaba <mwangindaba@gmail.com>
2022-10-03 21:54:57 +0300

    build: updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import path from "path";
@@ -21,6 +22,7 @@ export default defineConfig({
     react(),
     mdx(),
     image(),
+    sitemap(),
   ],
   vite: {
     resolve: {
