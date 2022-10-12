1900e707498fcc1ccd8ab0f24e20440542be4139
Victor Ndaba <mwangindaba@gmail.com>
2022-10-12 15:31:21 +0300

    refactor: converted navlinks from jsx to astro component

--- src/components/NavItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NavItem(props) {
-  return (
-    <a href={props.href} className="text-base font-medium hover:text-teal-500 transition mx-4 cursor-pointer">
-      {props.children}
-    </a>
-  );
-}
