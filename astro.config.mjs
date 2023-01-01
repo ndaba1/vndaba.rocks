bbcbf921a33882eeb8fd7457f77b0fc47b755557
Victor Ndaba <mwangindaba@gmail.com>
2023-01-01 17:49:39 +0300

    build: added partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,32 +6,29 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import path from "path";
 import { fileURLToPath } from "url";
+import partytown from "@astrojs/partytown";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vndaba.rocks",
   base: "/",
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    react(),
-    mdx(),
-    image(),
-    sitemap(),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), react(), mdx(), image(), sitemap(), partytown()],
   vite: {
     resolve: {
       alias: {
-        "~": path.resolve(__dirname, "./src"),
-      },
-    },
+        "~": path.resolve(__dirname, "./src")
+      }
+    }
   },
   markdown: {
-    syntaxHighlight: "prism",
-  },
+    syntaxHighlight: "prism"
+  }
 });
